b047e698b1e667b2e76d67e8f1821da8c3b56115
hkariman3 <hrmtk0@gmail.com>
2022-12-02 14:58:26 +0000

    ルーティング途中まで

---
+++ app/controllers/public/address_controller.rb
@@ -0,0 +1,18 @@
+class Public::AddressController < ApplicationController
+
+  def index
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :customers,skip: [:passwords], controllers: {
+
+root to :"homes#top"
+
+devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 
-  devise_for :admin,skip: [:registrations, :passwords] , controllers: {
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+resources :items, only: [:index, :show]
+
+namespace :admin do
+
+end
+
 end
