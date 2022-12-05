8fa8fc3141001ca01c3d7c0d56cddb6245af403a
hkariman3 <hrmtk0@gmail.com>
2022-12-05 13:48:47 +0000

    サインインOK

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-root to :"homes#top"
 
 devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
@@ -11,9 +10,30 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
+
+scope module: :public do
+root to:"homes#top"
+get "/about" => "homes#about"
+
 resources :items, only: [:index, :show]
 
+resources :customers, only: [:edit, :show, :update]
+get "/customers/unsubscribe" => "customers#unsubscribe"
+get "/customers/withdraw" => "customers#withdraw"
+
+resources :cart_items, only: [:index, :update, :create, :destroy]
+get "/cart_items/destroy_all" => "/cart_items#destroy_all"
+
+resources :orders, only: [:new, :index, :show]
+get "/orders/confirm" => "orders#confirm"
+get "/orders/complete" => "orders#complete"
+
+resources :address, only: [:index, :edit, :create, :update, :destroy]
+
+end
+
 namespace :admin do
+
 
 end
 
